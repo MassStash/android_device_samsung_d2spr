400f56b7392f0ac9c7ed34b8b3568f4de344a094
bigshotrob22 <rrascon22@gmail.com>
2013-10-30 14:28:27 -0700

    Remove common_cdma.mk import
    Change-Id: I1c3e108f097e52bea4644172d864a95a1f8f2f4d

--- carbon.mk
+++ carbon.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_cdma.mk)
-
-# Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # languages
