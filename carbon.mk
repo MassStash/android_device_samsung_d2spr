e3bc442721da1cb182b0dea5831a9b4fcb54238b
MassStash <MassStash@gmail.com>
2014-01-28 10:20:01 -0600

    updates

--- carbon.mk
+++ carbon.mk
@@ -9,8 +9,8 @@ PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA e
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
     TARGET_DEVICE=d2spr \
-    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.1.2/JZO54K/L710VPBMD4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2spr-user 4.1.2 JZO54K L710VPBMD4 release-keys"
+    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUCMK3:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUCMK3 release-keys"
 
 PRODUCT_NAME := carbon_d2spr
 PRODUCT_DEVICE := d2spr
